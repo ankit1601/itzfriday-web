c48ec9d43d29644ca6c01e5ba599ddf6b898bbb1
Gobinda Thakur <gobinda.thakur@outlook.com>
2016-12-01 12:19:02 +0530

    Adding authentication for chat

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -3,17 +3,29 @@ var defaultRoom = 'general';
 var rooms = ["General", "Developer"];
 var chatMessages = [];
 var participants = ['Ankit', 'Apurv'];
+var isValid = false;
 module.exports = function (socket) {
 
-	 // send the new user their name and a list of users
-  socket.emit('init', {
+  // send the new user their name and a list of users
+  /*socket.emit('init', {
     rooms: rooms,
     chatMessages: chatMessages,
     participants: participants
+  });*/
+// validate a user
+  socket.on('authenticating', function (data) {
+    console.log(data);
+    if(data ==='1234526'){
+      isValid = true;
+    }else {
+      isValid = false;
+    }
   });
 
 	// broadcast a user's message to other users
   socket.on('send:message', function (data) {
-    socket.broadcast.emit('send:message', data);
+    if(isValid){
+      socket.broadcast.emit('send:message', data);
+    }
   });
 }
