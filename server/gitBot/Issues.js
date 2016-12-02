d4b07dbc7f76657b5cd4979379ec3e6d21fce84c
aptDroid <apt.sharp@gmail.com>
2016-12-02 10:58:00 +0530

    updated gitBot port

--- server/gitBot/Issues.js
+++ server/gitBot/Issues.js
@@ -1,8 +1,11 @@
 var express = require('express');
-//var request = require('request');
 var request = require('superagent');
-
+var bodyParser = require('body-parser');
 var app = express();
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 var username = "aptDroid";
 var repo = "HTML5";
@@ -12,174 +15,131 @@ var labels = ["bug"];
 var authToken = "7f0ed551deb6af4ffe06174d17e3aed0294f98f9";
 var assignees = "aptDroid";
 var state = "open";
-var issueNumber = 27
+var issueNumber = 29;
 var comment = "testing commenT";
-
-var postData = JSON.stringify({'title': title,'body':body, 'labels': labels, 'assignee': assignees, 'state' : state});
-
+var postData = JSON.stringify({'title': 'title changed','body':body, 'labels': labels, 'assignee': assignees, 'state' : state});
+var flag=0;
 //create an issue
-// app.use("/CreateIssue",function(req,res,next){
-
-// 	request({
-// 		method: 'POST',
-// 		uri: 'https://api.github.com/repos/'+username+'/'+repo+'/issues?oauth_token='+authToken,
-// 		headers : {
-// 			'User-Agent': username,
-// 			'Content-Type': 'application/json'
-// 		},
-// 		body : JSON.stringify({'title': title,'body':body, 'labels': labels, 'assignee': assignees, 'state' : 'open'}),
-// 	},
-// 	function(error,response,body){
-// 		if(error)
-// 		{
-// 			return res.send(error);
-// 		}
-
-// 		 let data = JSON.parse(body);
-
-// 		return res.send("New issue has been opened with number : "+data.number);
-// 	});
-// });
-
-
 app.use("/CreateIssue",function(req,res,next){
-
-	request.post('https://api.github.com/repos/'+username+'/'+repo+'/issues?oauth_token='+authToken)
-	.set('User-Agent',username)
-	.set('Content-Type','application/json')
-	.send(postData)
-	.end(function(error,response){
-		if(error)
-		{
-			return res.send(error);
-		}
-		return res.send("New issue has been opened with number : ");
-	});
-	// request({
-	// 	method: 'POST',
-	// 	uri: 'https://api.github.com/repos/'+username+'/'+repo+'/issues?oauth_token='+authToken,
-	// 	headers : {
-	// 		'User-Agent': username,
-	// 		'Content-Type': 'application/json'
-	// 	},
-	// 	body : JSON.stringify({'title': title,'body':body, 'labels': labels, 'assignee': assignees, 'state' : 'open'}),
-	// },
-	// function(error,response,body){
-	// 	if(error)
-	// 	{
-	// 		return res.send(error);
-	// 	}
-
-	// 	let data = JSON.parse(body);
-
-	// 	return res.send("New issue has been opened with number : "+data.number);
-	// });
+    request.post('https://api.github.com/repos/'+username+'/'+repo+'/issues?oauth_token='+authToken)
+    .set('User-Agent',username)
+    .set('Content-Type', 'application/json')
+    .send(postData)
+    .end(function(error,response){
+        if(error)
+        {
+            return res.send(error);
+        }
+        var data=response.body;
+        return res.send("New issue has been opened with number "+data.number);
+    });
 });
-
-
 //edit an issue, use the same to tag and assign
 app.use("/EditIssue",function(req,res,next){
-
-	request({
-		method: 'PATCH',
-		uri: 'https://api.github.com/repos/'+username+'/'+repo+'/issues/'+issueNumber+'?oauth_token='+authToken,
-		headers : {
-			'User-Agent': username,
-			'Content-Type': 'application/json'
-		},
-		body : JSON.stringify({'title': 'title changed','body':body, 'labels': labels, 'assignee': assignees, 'state' : state}),
-	},
-	function(error,response,body){
-		if(error)
-		{
-			return res.send(error);
-		}
-
-		 let data = JSON.parse(body);
-
-		return res.send("Issue "+data.number+" has been updated successfully!");
-	});
+    request.patch('https://api.github.com/repos/'+username+'/'+repo+'/issues/'+issueNumber+'?oauth_token='+authToken)
+    .set('User-Agent',username)
+    .set('Content-Type','application/json')
+    .send(postData)
+    .end(function(error, response){
+        if(error)
+            res.send(error);
+        res.send("Issue "+response.body.number+" has been updated successfully!");
+    });
 });
-
+//close the issue
+app.use("/CloseIssue",function(req,res,next){
+    issueNumber = req.body.issue;
+    request.patch('https://api.github.com/repos/'+username+'/'+repo+'/issues/'+issueNumber+'?oauth_token='+authToken)
+    .set('User-Agent',username)
+    .set('Content-Type','application/json')
+    .send(JSON.stringify({'state':'closed'}))
+    .end(function(error, response){
+        if(error)
+            res.send(error);
+        flag="Issue "+response.body.number+" has been closed!";
+        res
+        .redirect('/ListAllIssues');
+    });
+});
 //list all issues of a repository
 app.use("/ListAllIssues",function(req,res,next){
-	let data = '';
-
-	request.get('https://api.github.com/repos/'+username+'/'+repo+'/issues')
-	.set('User-Agent',username)
-	.set('Content-Type','application/json')
-	.end(function(error,response)
-	{
-		if(error)
-			res.send(error);
-
-		let issuesList = '';
-
-		 for(let index in response.body)
-		 {
-		 	issuesList = issuesList + "<input type='checkbox' value='"+response.body[index]['number']+"'>"+response.body[index]['number']+" : "+response.body[index]['title']+"</input><br/>";
-		 }
-
-		return res.send(issuesList);
-	});
-
-
+    var data = '';
+    request.get('https://api.github.com/repos/'+username+'/'+repo+'/issues?')
+    .set('User-Agent',username)
+    .set('Content-Type','application/json')
+    .end(function(error,response)
+    {
+        if(error)
+            res.send(error);
+        var issuesList = '';
+        var str='';
+        for(var index in response.body)
+        {
+            issuesList = issuesList + "<input type='checkbox' name='issue' value='"+response.body[index]['number']+"' onClick='handleCheck(this)'/>"+
+            response.body[index]['number']+" : "+response.body[index]['title']+"<br/>";
+        }
+        if(flag !== 0)
+        {
+          str=flag;
+          flag=0;
+        }
+        return res.send("<div><form action='/CloseIssue' method='POST'><div style='height: 300px; min-width:400px; overflow-y: scroll;'>"+
+            issuesList +
+            "</div><input type='submit' id='submitBtn' value='Close &times;' disabled='true' style='cursor:not-allowed;color:grey;border: none;padding: 5px 5px 5px 5px; font-size: 14px; background-color: transparent; text-decoration: underline'/>"+
+            "</form></div>"+
+            "<script>"+
+            "const handleCheck = function (target)"+
+            "{ var children = target.parentElement.children;"+
+                "for(let index in children)"+
+                "{"+
+                    "if(children[index].value !== target.value)"+
+                        "children[index].checked=false"+
+                "}"+
+                "if(!target.checked)"+
+                "{"+
+                    "document.getElementById('submitBtn').disabled = true;"+
+                    "document.getElementById('submitBtn').style.color='grey';"+
+                    "document.getElementById('submitBtn').style.cursor='not-allowed';"+
+                    "}"+
+                "else "+
+                "{"+
+                    "document.getElementById('submitBtn').disabled = false;"+
+                    "document.getElementById('submitBtn').style.color='blue';"+
+                    "document.getElementById('submitBtn').style.cursor='pointer';"+
+                    "}"+
+            "}"+
+            "</script><br/>"+
+            "<span>"+str+"</span>"
+                      );
+    });
 });
-
-
 //comment on an issue
 app.use("/CommentOnAnIssue",function(req,res,next){
-
-	request({
-		method: 'POST',
-		uri: 'https://api.github.com/repos/'+username+'/'+repo+'/issues/'+issueNumber+'/comments?oauth_token='+authToken,
-		headers : {
-			'User-Agent': username,
-			'Content-Type': 'application/json'
-		},
-		body : JSON.stringify({'body':comment}),
-	},
-	function(error,response,body){
-		if(error)
-		{
-			return res.send(error);
-		}
-
-		 let data = JSON.parse(body);
-		
-		return res.send('Comment posted successfully! <br/>Your comment id is '+data.id);
-	});
+    request.post('https://api.github.com/repos/'+username+'/'+repo+'/issues/'+issueNumber+'/comments?oauth_token='+authToken)
+    .set('User-Agent',username)
+    .set('Content-Type','application/json')
+    .send(JSON.stringify({'body':comment}))
+    .end(function(error,response)
+    {
+        if(error)
+            res.send(error);
+        res.send('Comment posted successfully! <br/>Your comment id is '+response.body.id);
+    });
 });
-
 //list comments on an issue
 app.use("/ListCommentsOnAnIssue",function(req,res,next){
-	request({
-		uri: 'https://api.github.com/repos/'+username+'/'+repo+'/issues/'+issueNumber+'/comments',
-		headers : {
-			'User-Agent': username,
-			'Content-Type': 'application/json',
-			'X-Auth-Token': 'a192dfd5d72225a5f8780bf5d608623ddbdeab30'
-		}
-	},
-	function(error, response, body)
-	{
-		if(error)
-		{
-			console.error(error);
-			return res.send(error);
-		}
-
-		let commentList = '';
-
-		let data = JSON.parse(body);
-
-		for(let index in data)
-		{
-			commentList = commentList + (Number(index)+1)+"<label value='"+data[index]['id']+"'> : "+data[index]['body']+"</label> <sub>~"+data[index]['user']['login']+"</sub><br/>";
-		}
-
-		return res.send(commentList);
-	})
+    request.get('https://api.github.com/repos/'+username+'/'+repo+'/issues/'+issueNumber+'/comments')
+    .set('User-Agent',username)
+    .set('Content-Type','application/json')
+    .end(function(error,response){
+        if(error)
+            res.send(error);
+        var commentList = '';
+        for(var index in response.body)
+        {
+            commentList = commentList + (Number(index)+1)+"<label value='"+response.body[index]['id']+"'> : "+response.body[index]['body']+"</label> <sub>~"+response.body[index]['user']['login']+"</sub><br/>";
+        }
+        return res.send(commentList);
+    });
 });
-
-app.listen(3000);
+app.listen(3001);
